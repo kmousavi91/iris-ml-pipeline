35cda37c984b7c5bc73926770f89b605530dd1b7
Kourosh (Mohammad) Mousavi <kouroshmousavi1@gmail.com>
2025-03-26 16:06:20 +0100

    Fix test.py to use fit_model for CI

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-# test.py
-from scripts.train import train_model
 import numpy as np
+from scripts.train import fit_model
 
 def test_model_shape():
     X_train = np.random.rand(10, 4)
     y_train = np.random.randint(0, 3, 10)
-    model = train_model(X_train, y_train)
-    assert hasattr(model, "predict"), "Model should have predict method"
+    
+    model = fit_model(X_train, y_train)
+    
+    assert hasattr(model, "predict")
+    assert model.n_classes_ == 3
+
